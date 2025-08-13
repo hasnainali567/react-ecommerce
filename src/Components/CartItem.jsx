8a8a53d316e92b81a5c89ee6d107d4522698f7fe
Hasnain Ali <hasnain5f7@gmail.com>
2025-08-13 09:55:10 +0500

    Update CartItem.jsx

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,6 +1,23 @@
-import React from "react";
-
+import React, {useState} from "react";
+import { useDispatch } from "react-redux";
+import { deleteCart } from "./Features/UserSlice";
+import { auth } from "../Firebase/Firebase";
 const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(false);
+
+
+  const handleDel= ()=>{
+    console.log('handleDel');
+    
+    setLoading(true)
+    const userId = auth.currentUser.uid;
+    dispatch(deleteCart({delItem : item, userId}))
+    setLoading(false)
+  }
+
+
   return (
     <div className="flex items-center justify-between rounded-lg border-2 shadow border-light-secondary p-3">
       <img src={item.image} alt={item.title} className="w-18 h-18 object-cover rounded-md overflow-hidden" />
@@ -9,7 +26,7 @@ const CartItem = ({ item }) => {
         <p className="text-sm text-gray-400">${item.price}</p>
         <p className="text-sm text-gray-500">Quantity: {item.quantity}</p>
       </div>
-      <button className="bg-light-text cursor-pointer text-white px-4 py-2 rounded-md">Remove</button>
+      <button disabled={loading} onClick={handleDel} className="bg-light-text cursor-pointer text-white px-4 py-2 rounded-md">Remove</button>
     </div>
   );
 };
